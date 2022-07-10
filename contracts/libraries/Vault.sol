724c0b8a851541402b288186132ee9adf25cb22d
mhw32 <me@mikewuis.me>
2022-07-10 13:32:59 -0700

    Updates to libraries

--- contracts/libraries/Vault.sol
+++ contracts/libraries/Vault.sol
@@ -2,32 +2,37 @@
 pragma solidity >=0.8.6;
 
 /**
- * @notice Collection of constants and structs describing a theta vault
+ * @notice Collection of constants and structs describing a theta vault.
+ * @dev Used thoroughly in `ParetoVault` and `ParetoManager`
  */
 library Vault {
-    // Fees are 6-decimal places. For example: 20 * 10**6 = 20%
+    /// @notice Fees are 6-decimal places. For example: 20 * 10**6 = 20%
     uint256 internal constant FEE_DECIMALS = 6;
 
+    /**
+     * @notice State of the current and next RMM-01 pool 
+     * @param nextPoolId Primitive pool that the vault is depositing into next cycle
+     * @param currPoolId Primitive pool that the vault is currently depositing into
+     * @param currLiquidity Amount of liquidity deposited into the current pool
+     * @param currPoolParams Black scholes parameters for the current pool
+     * @param nextPoolParams Black scholes parameters for the next pool
+     * @param nextPoolReadyAt The timestamp when the `nextPoolId` can be used by the vault
+     */
     struct PoolState {
-        // Primitive pool that the vault is depositing into next cycle
         bytes32 nextPoolId;
-        // Primitive pool that the vault is currently depositing into
         bytes32 currPoolId;
-        // Amount of liquidity deposited into the current pool
         uint256 currLiquidity;
-        // Black scholes parameters for the current pool
         PoolParams currPoolParams;
-        // Black scholes parameters for the next pool
         PoolParams nextPoolParams;
-        // The timestamp when the `nextPoolId` can be used by the vault
         uint32 nextPoolReadyAt;
     }
 
     /**
-     * @param risky is the address of the risky asset
-     * @param stable is the address of the stable asset
-     * @param riskyDecimals is the decimals for the risky asset
-     * @param stableDecimals is the decimals for the stable asset
+     * @notice Parameters for tokens used in the vault
+     * @param risky Address of the risky asset
+     * @param stable Address of the stable asset
+     * @param riskyDecimals Decimals for the risky asset
+     * @param stableDecimals Decimals for the stable asset
      */
     struct TokenParams {
         address risky;
@@ -37,15 +42,17 @@ library Vault {
     }
 
     /**
-     * @param spotAtCreation is the spot price at the time the pool was created
-     *                       Used to compute riskyPerLp (and thus stablePerLp)
-     * @param strike is the strike price of the pool
-     * @param sigma is the implied volatility of the pool
-     * @param maturity is the timestamp when the option pool expires
-     * @param gamma is the gamma of the pool (1 - fee)
-     * @param riskyPerLp is the risky reserve per liq. with risky decimals,
-     *  = 1 - N(d1), d1 = (ln(S/K)+(r*sigma^2/2))/sigma*sqrt(tau)
-     * @param stablePerLp is the stable reserve per liq. with stable decimals
+     * @notice Parameters of the RMM-01 pool
+     * @param spotAtCreation Spot price at the time the pool was created
+     *                       Used to compute `riskyPerLp` (and thus `stablePerLp`)
+     * @param strike Strike price of the pool
+     * @param sigma Implied volatility of the pool
+     * @param maturity Timestamp when the option pool expires
+     * @param gamma Gamma of the pool (1 - fee)
+     * @param riskyPerLp Risky reserve per liquidity with risky decimals
+     * @param stablePerLp Stable reserve per liq. with stable decimals
+     * @dev The variable `RiskyPerLp` (R1) is computed as `R1 = 1 - N(d1)`.
+     *      Also, `d1 = (ln(S/K)+(r*sigma^2/2))/sigma*sqrt(tau)`
      */
     struct PoolParams {
         uint128 spotAtCreation;
@@ -58,12 +65,13 @@ library Vault {
     }
 
     /**
-     * @param manualStrike is a manually specified strike price
-     * @param manualStrikeRound is the round of a manual strike
-     * @param manualVolatility is a manually specified IV
-     * @param manualVolatilityRound is the round of a manual IV
-     * @param manualGamma is a manually specified fee rate
-     * @param manualGammaRound is the round of a manual fee rate
+     * @notice State used to override manager parameters for the next pool
+     * @param manualStrike Manually specified strike price
+     * @param manualStrikeRound Round of a manual strike
+     * @param manualVolatility Manually specified IV
+     * @param manualVolatilityRound Round of a manual IV
+     * @param manualGamma Manually specified fee rate
+     * @param manualGammaRound Round of a manual fee rate
      */
     struct ManagerState {
         uint128 manualStrike;
@@ -75,25 +83,20 @@ library Vault {
     }
 
     /**
-     * @param round is the current round number
-     * @param lockedRisky is the amount of risky asset locked away in
-     *  a covered call short position
-     * @param lockedStable is the amount of stable asset locked away in
-     *  a covered call short position
-     * @param lastLockedRisky is the amount of risky asset locked from last
-     *  round; used to compute performance fee
-     * @param lastLockedStable is the amount of stable asset locked from last
-     *  round; used to compute performance fee
-     * @param pendingRisky is the amount of risky asset to be used to mint
-     *  receipt tokens
-     * @param lastQueuedWithdrawRisky is the qmount of risky asset locked for
-     *  withdrawal last vault
-     * @param lastQueuedWithdrawStable is the amount of stable asset locked for
-     *  withdrawal last vault
-     * @param currQueuedWithdrawShares is the amount of shares locked for
-     *  withdrawal currently
-     * @param totalQueuedWithdrawShares is the amount of shares locked for
-     *  withdrawal in all previous rounds (not including current)
+     * @notice State of the vault, containing locked and pending assets 
+     *         along with assets queued for withdrawal
+     * @param round Current round number
+     * @param lockedRisky Amount of risky asset locked away in a covered call short position
+     * @param lockedStable Amount of stable asset locked away in a covered call short position
+     * @param lastLockedRisky Amount of risky asset locked from last round.
+     *                        Used to compute performance fee
+     * @param lastLockedStable Amount of stable asset locked from last round.
+                               Used to compute performance fee
+     * @param pendingRisky Amount of risky asset to be used to mint receipt tokens
+     * @param lastQueuedWithdrawRisky Amount of risky asset received from withdrawal last round 
+     * @param lastQueuedWithdrawStable Amount of stable asset received from withdrawal last round
+     * @param currQueuedWithdrawShares Amount of shares locked for withdrawal this round
+     * @param totalQueuedWithdrawShares Amount of shares locked for withdrawal over all users
      */
     struct VaultState {
         uint16 round;
@@ -109,12 +112,11 @@ library Vault {
     }
 
     /**
-     * @param round is the round number with a maximum of 65535 rounds
-     *  Assuming round is 1 week, max is 1256 yrs
-     * @param riskyToDeposit is the deposit amount of the risky asset
-     *  to be converted into shares at rollover
-     * @param ownedShares is the amount of shares owned by user from
-     *  depositing in past rounds
+     * @notice Receipt given to users after a deposit
+     * @param round Round number with a maximum of 65535 rounds.
+     *              Assuming round is 1 week, max is 1256 yrs
+     * @param riskyToDeposit Amount of the risky asset to be converted into shares at rollover
+     * @param ownedShares Amount of shares owned by user from depositing in past rounds
      */
     struct DepositReceipt {
         uint16 round;
@@ -123,8 +125,9 @@ library Vault {
     }
 
     /**
-     * @param round is the round number with a maximum of 65535 rounds
-     * @param shares is the number of withdrawn shares
+     * @notice Structure that keeps track of when to and how much shares to withdraw
+     * @param round Round number with a maximum of 65535 rounds
+     * @param shares Number of withdrawn shares
      */
     struct PendingWithdraw {
         uint16 round;
@@ -132,13 +135,14 @@ library Vault {
     }
 
     /**
-     * @param currRisky is the balance of risky assets in vault
-     * @param currStable is the balance of stable assets in vault
-     * @param lastLockedRisky is the amount of risky assets locked from last round
-     * @param lastLockedStable is the amount of stable assets locked from last round
-     * @param pendingRisky is the pending deposit amount of risky asset
-     * @param managementFeePercent is the fee percent on the AUM in both assets
-     * @param performanceFeePercent is the fee percent on the premium in both assets
+     * @notice Input structure to compute fees
+     * @param currRisky Balance of risky assets in vault
+     * @param currStable Balance of stable assets in vault
+     * @param lastLockedRisky Amount of risky assets locked from last round
+     * @param lastLockedStable Amount of stable assets locked from last round
+     * @param pendingRisky Pending deposit amount of risky asset
+     * @param managementFeePercent Fee percent on the AUM in both assets
+     * @param performanceFeePercent Fee percent on the premium in both assets
      */
     struct FeeCalculatorInput {
         uint256 currRisky;
@@ -151,10 +155,11 @@ library Vault {
     }
 
     /**
-     * @param preVaultRisky is the amount of risky token the vault owns at the start of the round
-     * @param preVaultStable is the amount of stable token the vault owns at the start of the round
-     * @param postVaultRisky is the amount of risky token the vault owns at the end of the round
-     * @param postVaultStable is the amount of stable token the vault owns at the end of the round
+     * @notice Input structure to the `_checkVaultSuccess` function
+     * @param preVaultRisky Amount of risky token the vault owns at the start of the round
+     * @param preVaultStable Amount of stable token the vault owns at the start of the round
+     * @param postVaultRisky Amount of risky token the vault owns at the end of the round
+     * @param postVaultStable Amount of stable token the vault owns at the end of the round
      */
     struct VaultSuccessInput {
         uint256 preVaultRisky;
@@ -164,8 +169,9 @@ library Vault {
     }
 
     /**
-     * @param router is the address for the Uniswap router contract
-     * @param poolFee for swaps in uniswap pool to search for
+     * @notice Parameters for the Uniswap swap AMM
+     * @param router Address for the Uniswap router contract
+     * @param poolFee Fee for the uniswap pool to search for when making a swap
      */
     struct UniswapParams {
         address router;
@@ -173,10 +179,11 @@ library Vault {
     }
 
     /**
-     * @param manager is the address for the Primitive manager contract
-     * @param engine is the address for the Primitive engine contract
-     * @param factory is the address for the Primitive factory contract
-     * @param decimals is the decimals for Primitive liquidity
+     * @notice Parameters for the Primitive contracts
+     * @param manager Address for the Primitive manager contract
+     * @param engine Address for the Primitive engine contract
+     * @param factory Address for the Primitive factory contract
+     * @param decimals Decimals for Primitive liquidity
      */
     struct PrimitiveParams {
         address manager;
